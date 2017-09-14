d16a41bc781b6945140ed38de1fa9b2111723a38
rudcks91 <rudcks1991@gmail.com>
2017-09-13 22:36:18 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,58 +19,63 @@ $(document).ready(function(){
     $("#crystal4").click(crystalvalue4);
 
     function reset() {
-        var crystal1 = Math.floor(Math.random()*(11)+1);
-        var crystal2 = Math.floor(Math.random()*(11)+1);
-        var crystal3 = Math.floor(Math.random()*(11)+1);
-        var crystal4 = Math.floor(Math.random()*(11)+1);
+        crystal1 = Math.floor(Math.random()*(11)+1);
+        crystal2 = Math.floor(Math.random()*(11)+1);
+        crystal3 = Math.floor(Math.random()*(11)+1);
+        crystal4 = Math.floor(Math.random()*(11)+1);
         total = 0;
         randomnumber = Math.floor(Math.random()*(100)+19);
+        $("#randombox").html("<h1>" + randomnumber + "</h1>");
+        $("#scorebox").html("0");
     }
 
     function crystalvalue1() {
         total = total + crystal1;
         console.log("total1  "+total);
         console.log("crystal1  "+crystal1);
-            $("#scorebox").text(total);
+        $("#scorebox").text(total);
+        score();
     }
 
     function crystalvalue2() {
         total = total + crystal2;
         console.log("total1  "+total);
         console.log("crystal2  "+crystal2);
-            $("#scorebox").text(total);
+        $("#scorebox").text(total);
+        score();
     }
 
     function crystalvalue3() {
         total = total + crystal3;
         console.log("total1  "+total);
         console.log("crystal3  "+crystal3);
-            $("#scorebox").text(total);
+        $("#scorebox").text(total);
+        score();
     }
 
     function crystalvalue4() {
         total = total + crystal4;
         console.log("total1  "+total);
         console.log("crystal4  "+crystal4);
-            $("#scorebox").text(total);
+        $("#scorebox").text(total);
+        score();
     }
 
-
-
-if (total === randomnumber) {
+    function score(){
+    if (total === randomnumber) {
     wins ++;
-    $("#wins").text(wins);
-    reset();
+    $("#wins").html(wins);
     alert("win");
-    console.log(wins);
-}
-else if (total > randomnumber) {
-    losses ++;
-    $("#losses").text(losses);
     reset();
-    alert("loss");
-    console.log(losses);
 }
+
+    else if (total > randomnumber) {
+    losses ++;
+    $("#losses").html(losses);
+    alert("loss");
+    reset();
+}
+    }
 
 
 });
